33168d65baabca4797f17a3a5f45dd83f3d9d9cb
Navleen Gill <navleengill1661@gmail.com>
2017-02-07 15:52:40 -0500

    Modified the index.js file
    Added code to get the home, contact, about and projects page

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Portfolio' });
 });
 
+/* GET home page */
+router.get('/home', function (req, res, next) {
+   res.render('home');
+});
+
+/* GET about page */
+router.get('/about', function (req, res, next) {
+    res.render('about');
+});
+
+/* GET projects page */
+router.get('/projects', function (req, res, next) {
+    res.render('projects');
+});
+
+/* GET contact page */
+router.get('/contact', function (req, res, next) {
+    res.render('contact');
+});
+
 
 module.exports = router;
